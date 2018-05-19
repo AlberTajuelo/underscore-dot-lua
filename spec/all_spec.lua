61960713c5ca50801f28f838204e7b675772cf1a
Alberto Gonzalez Tajuelo <albertajuelo@gmail.com>
2018-05-19 12:11:17 +0200

    Improvements: more doc, added luacov, luacheck, moved src, edited LICENSE, improved gitignore, all_spec fixed

--- spec/all_spec.lua
+++ spec/all_spec.lua
@@ -1,55 +1,45 @@
-require 'spec_helper'
+local _ = require("src/underscore")
 
-describe["_.all"] = function()
-	describe["when providing a truth function"] = function()
-		describe["when all elements pass the function"] = function()
-			before = function()
-				input = { 2,4,6 }
-				result = _.all(input, function(i) return i%2==0 end)
-			end
-	
-			it["should return true"] = function()
-				expect(result).should_be(true)
-			end
-		end
-		
-		describe["when all elements don't pass the function"] = function()
-			before = function()
-				input = { 1,2,3 }
-				result = _.all(input, function(i) return i%2==0 end)
-			end
-	
-			it["should return false"] = function()
-				expect(result).should_be(false)
-			end
-		end
-	end
+describe("#all", function()
 
-	describe["when not providing a truth function"] = function()
-		describe["when all elements are not false"] = function()
-			before = function()
-				input = { 1,2,true }
-				result = _.all(input)
-			end
-	
-			it["should return true"] = function()
-				expect(result).should_be(true)
-			end
-		end
-		
-		describe["when some elements are not true"] = function()
-			before = function()
-				input = { 1,2,3,false }
-				result = _.all(input)
-			end
-	
-			it["should return false"] = function()
-				expect(result).should_be(false)
-			end
-		end
-	end
+  describe("provide a truth function", function()
+    describe("all elements pass the function", function()
+      it("should return true", function()
+        input = {2, 4, 6}
+        result = _.all(input, function(i) return i % 2 == 0 end)
+        assert.same(result, true)
 
-end
+      end)
+    end)
 
+    describe("when all elements don't pass the function", function()
+      it("should return false", function()
+        input = {1, 2, 3}
+        result = _.all(input, function(i) return i % 2 == 0 end)
+        assert(not result)
 
-spec:report(true)
+      end)
+    end)
+  end)
+
+  describe("when not providing a truth function", function()
+    describe("when all elements are not false", function()
+      it("should return true", function()
+        input = {1, 2, true}
+        result = _.all(input)
+
+        assert(result)
+      end)
+    end)
+    
+    describe("when some elements are not true", function()
+      it("should return false", function()
+        input = {1, 2, 3, false}
+        result = _.all(input)
+
+        assert(not result)
+      end)
+    end)
+  end)
+
+end)
